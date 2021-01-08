fe0312bcd9dd3e4e7b35a30af385187c70aa3ef2
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2021-01-07 22:39:50 -0500

    Depend on PATH-available steamcmd and add CheckLogin

--- steamcmd.go
+++ steamcmd.go
@@ -1,7 +1,6 @@
 package steamcmd
 
 import (
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -31,28 +30,13 @@ type SteamCmd struct {
 
 // New creates a new steamcmd instance, if path is empty, a temporary
 // path will be created. Otherwise an existing instance will be reused.
-func New(user, pass, path string) *SteamCmd {
+func New(user, pass) *SteamCmd {
 	var err error
 
 	// if user not specified correctly, default to anonymous login
 	if user == "" || pass == "" {
 		user = "anonymous"
 		pass = ""
-	}
-
-	// if path was left empty, we create a temporary dir for steam
-	if path == "" {
-		path, err = ioutil.TempDir("", "steamcmd")
-		if err != nil {
-			err = errors.Wrap(err, "Could not create steamcmd temp dir")
-			panic(err)
-		}
-	}
-
-	gamesPath, err := ioutil.TempDir("", "games")
-	if err != nil {
-		err = errors.Wrap(err, "Could not create apps temp dir")
-		panic(err)
 	}
 
 	scmd := &SteamCmd{
@@ -70,37 +54,11 @@ func (scmd SteamCmd) CheckLogin() error {
 	return scmd.run("")
 }
 
-// EnsureInstalled checks if the SteamCmd is executable, and bootstraps it otherwise.
+// EnsureInstalled checks if the SteamCmd is executable.
 // Remember, steam needs curl, bzip2, tar and lib32gcc1
 func (scmd SteamCmd) EnsureInstalled() error {
-	const SteamCmdURL = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
-
-	// check if SteamCmd needs to be bootstrapped
-	fi, err := os.Stat(filepath.Join(scmd.SteamCmdDir, "steamcmd.sh"))
-
-	// check whether it does not exist or is not executable
-	if err != nil || (fi.Mode()&0111 == 0) {
-		// we should bootstrap it again
-		if err := os.MkdirAll(scmd.SteamCmdDir, 0755); err != nil {
-			return errors.Wrap(err, "Could not create steam path")
-		}
-
-		task := exec.Command("bash", "-c", "curl -sqL \""+SteamCmdURL+"\" | tar xzvf -")
-
-		task.Dir = scmd.SteamCmdDir
-
-		if scmd.Debug {
-			task.Stdout = os.Stdout
-			task.Stderr = os.Stderr
-		}
-
-		err := task.Run()
-		if err != nil {
-			return errors.Wrap(err, "SteamCmd download failed")
-		}
-
-	}
-	return nil
+	_, err := exec.LookPath("steamcmd")
+	return err
 }
 
 // GetAppPath returns the path where an app would be installed
@@ -110,8 +68,7 @@ func (scmd *SteamCmd) GetAppPath(id int) string {
 
 // InstallUpdateApp installs and updates a given app
 func (scmd *SteamCmd) InstallUpdateApp(id int) error {
-	return scmd.run("+force_install_dir", scmd.GetAppPath(id),
-		"+app_update", strconv.Itoa(id), "validate")
+	return scmd.run("+app_update", strconv.Itoa(id))
 }
 
 // AppInstalledVersion returns the Build ID of an Steam App
@@ -127,8 +84,7 @@ func (scmd *SteamCmd) AppAvailableVersion(id int) (int, error) {
 
 // DownloadWorkshopMod tries to download a mod from the workshop
 func (scmd *SteamCmd) DownloadWorkshopMod(appid, id int) error {
-	return scmd.run("+force_install_dir", scmd.GetAppPath(appid),
-		"+workshop_download_item", strconv.Itoa(appid), strconv.Itoa(id), "validate")
+	return scmd.run("+workshop_download_item", strconv.Itoa(appid), strconv.Itoa(id))
 }
 
 // run helper
@@ -143,8 +99,7 @@ func (scmd *SteamCmd) run(params ...string) error {
 	}
 	params = append(params, "+quit")
 
-	task := exec.Command("./steamcmd.sh", params...)
-	task.Dir = scmd.SteamCmdDir
+	task := exec.Command("steamcmd", params...)
 	if scmd.Debug {
 		task.Stdout = os.Stdout
 		task.Stderr = os.Stderr
